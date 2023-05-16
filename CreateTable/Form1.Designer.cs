f67e5ef40cf880b05364de180381a6fcfa7ff23b
CarinaChen423 <115776828+CarinaChen423@users.noreply.github.com>
2023-05-16 21:34:46 +0200

    Add files via upload

---
+++ CreateTable/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CreateTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(69, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(372, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CREATE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(96, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ROW";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Info;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(12, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(461, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SWAP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(242, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "COLUMN";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(227, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 560);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CreateTable/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateTable
+{
+    public partial class Form1 : Form
+    {
+        int tbX=60, tbY=150;
+        TextBox[,] allTextBoxes;
+        int rowNum, columnNum;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < rowNum; i++)
+            {
+                for(int j=0;j<columnNum; j+=2)
+                {
+                    string val1 = allTextBoxes[i, j].Text;
+                    string val2 = allTextBoxes[i, j+1].Text;
+                    allTextBoxes[i, j].Text = val2;
+                    allTextBoxes[i, j+1].Text = val1;
+                    //swap each two value
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int r = Convert.ToInt32(textBox1.Text);
+            int c = Convert.ToInt32(textBox2.Text);
+            
+            if (c % 2 == 0)
+            {
+                rowNum = r;
+                columnNum = c;
+                allTextBoxes = new TextBox[r, c];
+                for (int i = 1; i <= r; i++)
+                {
+                    for (int j = 1; j <= c; j++)
+                    {
+                        TextBox tb = new TextBox();
+                        tb.Width = 60;
+                        tb.Font = new Font("Arial", 10);
+                        tb.Location = new Point(tbX, tbY);
+                        tb.Name = $"tb{i}{j}";
+                        tb.Text = "";
+                        this.Controls.Add(tb);
+                        allTextBoxes[i - 1, j - 1] = tb; 
+                        tbX += 80;
+                    }
+                    tbX = 60;
+                    tbY += 50;
+                }
+                Button btn=new Button();
+                btn.Text = "Swap";
+                btn.Font = new Font("Arial", 24);
+                btn.Dock = DockStyle.Bottom;
+                btn.Height = 50;
+                btn.Click += button2_Click;
+                //btn.Click += new EventHandler(button2_Click);
+                this.Controls.Add(btn);
+            }
+            else
+            {
+                MessageBox.Show("Column should be even number!");
+            }
+        }
+    }
+}
+
